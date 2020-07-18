5f0652d316aa0103cb521d3eae2b2a33c54f0929
Michael Hamill <michael.hamill3@gmail.com>
2020-07-18 13:05:20 -0400

    Updates

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Select DOM Items
+const menu = document.querySelector('#menu-main');
+const menuBtn = document.querySelector('#menu-btn-main');
+const menuBranding = document.querySelector('#menu-branding-main');
+const menuNav = document.querySelector('#menu-nav-main');
+const navItems = document.querySelectorAll('.menu-item');
+
+// Set initial state of the menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach((item) => item.classList.add('show'));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach((item) => item.classList.remove('show'));
+    showMenu = false;
+  }
+}
